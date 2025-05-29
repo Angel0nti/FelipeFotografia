a184023ac52531cf38a15e5f56dd3e033a324093
Angel0nti <angel0ntialv21@gmail.com>
2025-05-28 18:41:52 -0700

    spinner solo en lazy img, cambio usando flex en contenedores de event y about me, animacion en grid

--- src/js/script2.js
+++ src/js/script2.js
@@ -249,9 +249,9 @@ class ImageSlider {
   }
 
   _renderSlides(images) {
-    this.spinner.classList.remove('hidden');
+    // this.spinner.classList.remove('hidden');
     this.container.innerHTML = '';
-    this.container.appendChild(this.spinner);
+    // this.container.appendChild(this.spinner);
 
     this.currentSlide;
     this.currentImages = images;
@@ -288,7 +288,7 @@ class ImageSlider {
           const img = entry.target;
           img.src = img.dataset.src;
           img.onload = () => {
-            this.spinner.classList.add('hidden');
+            // this.spinner.classList.add('hidden');
             img.classList.remove('lazy-img');
           };
           observer.unobserve(img);
@@ -352,7 +352,7 @@ class ImageSlider {
 }
 const slider = new ImageSlider({
   containerSelector: '.slides-container',
-  spinnerSelector: '.spinner',
+  // spinnerSelector: '.spinner',
   buttonsSelector: '.btn-image-slide',
   btnLeftSelector: '.slider__btn--left',
   btnRightSelector: '.slider__btn--right',
